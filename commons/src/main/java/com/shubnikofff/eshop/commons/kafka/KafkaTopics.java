aa4f0b270c370dd9151693c2afdaa5c6d235350a
Alexey Shubnikov <shubnikov.av@gmail.com>
2022-08-08 22:49:21 +0300

    Created module for commons

---
+++ commons/src/main/java/com/shubnikofff/eshop/commons/kafka/KafkaTopics.java
@@ -0,0 +1,9 @@
+package com.shubnikofff.eshop.commons.kafka;
+
+public final class KafkaTopics {
+
+	private KafkaTopics() {}
+
+	public static final String ACCOUNT_COMMAND_TOPIC = "account.commands.v1";
+	public static final String CUSTOMER_COMMAND_TOPIC = "customer.commands.v1";
+}
