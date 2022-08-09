aa3b484ecf3e445e360125c8ee44ad48bf632248
Alexey Shubnikov <alexey.shubnikov@monese.com>
2022-08-09 17:52:50 +0300

    Updated kafka topics in commons module

--- commons/src/main/java/com/shubnikofff/eshop/commons/kafka/topic/KafkaTopics.java
+++ commons/src/main/java/com/shubnikofff/eshop/commons/kafka/topic/KafkaTopics.java
@@ -4,6 +4,10 @@ public final class KafkaTopics {
 
 	private KafkaTopics() {}
 
-	public static final String ACCOUNT_COMMAND_TOPIC = "account.commands.v1";
-	public static final String CUSTOMER_COMMAND_TOPIC = "customer.commands.v1";
+	// commands
+	public static final String ACCOUNT_COMMAND_TOPIC = "account.command.v1";
+	public static final String CUSTOMER_COMMAND_TOPIC = "customer.command.v1";
+
+	// events
+	public static final String CUSTOMER_EVENT_TOPIC = "customer.event.v1";
 }
